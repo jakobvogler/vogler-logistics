c8fe9f47ba3a713571d4a5e1d14021951bb34e07
Jakob Christoph Vogler <jakob@jcvogler.com>
2023-10-02 00:06:28 +0200

    feat: update environment

--- Docker.sh
+++ Docker.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 echo "setenv MYSQL_URL ${MYSQL_URL}" >> /etc/apache2/apache2.conf
+echo "setenv DB_NAME ${DB_NAME}" >> /etc/apache2/apache2.conf
 echo "setenv ADMIN_USERNAME ${ADMIN_USERNAME}" >> /etc/apache2/apache2.conf
 echo "setenv ADMIN_PASSWORD ${ADMIN_PASSWORD}" >> /etc/apache2/apache2.conf
 echo "setenv CUSTOMER_USERNAME ${CUSTOMER_USERNAME}" >> /etc/apache2/apache2.conf
