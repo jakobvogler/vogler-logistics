9495fc50aec2f98cf7e5e9eb5df03bd6799584c2
Jakob Christoph Vogler <jakob@jcvogler.com>
2023-10-02 00:08:28 +0200

    fix: secure database connection

--- htdocs/inc/db.php
+++ htdocs/inc/db.php
@@ -2,8 +2,9 @@
     error_reporting(0);
 
     function connect($user, $password) {
-        $db = new mysqli(getenv("MYSQL_URL"), $user, $password, 'vogler_logistics', 3306);
-        $db->set_charset('utf8');
+        $db = mysqli_init();
+        $db->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
+        $db->real_connect(getenv("MYSQL_URL"), $user, $password, getenv("DB_NAME"));
 
         if ($db->connect_error) {
             die('Sorry - Something did not work');
